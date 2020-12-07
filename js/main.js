89286657e2b35711a707a68d876a595abdd2935e
iamnabholz <iamnabholz@gmail.com>
2020-12-07 13:05:12 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+new Splide('.splide', {
+  type: 'loop',
+  gap: "6vw",
+  pagination: false,
+  arrows: true,
+  autoplay: true,
+  resetProgress: true,
+  pauseOnHover: false,
+}).mount();
+
+document.onload = setButton();
+
+function setButton() {
+  const agent = navigator.userAgent;
+  var name;
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent)) {
+    document.getElementById("browser-button").style.display = "none";
+    document.getElementById("other-button").style.display = "block";
+  } else {
+
+    if (agent.includes("Edg")) {
+      name = "Edge";
+    } else if (agent.includes("Firefox")) {
+      name = "Firefox"
+    } else if (agent.includes("Chrome") && agent.includes("Edg") == false) {
+      name = "Chrome"
+    } else {
+      name = "other"
+    }
+
+    if (name == "other") {
+      document.getElementById("browser-button").style.display = "none";
+      document.getElementById("other-button").style.display = "block";
+    } else {
+      document.getElementById("browser").innerHTML = "Download For " + name;
+      document.getElementById("browser-icon").src = "img/" + name.toLowerCase() + ".png";
+    }
+  }
+}
+
+function stuff() {
+  console.log("hello");
+}
